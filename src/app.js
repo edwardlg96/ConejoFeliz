ba88800cff3a6739f126a5f2fc57d99e2a696e72
Edward García Ramírez <Edward Garca Ramrez>
2016-03-15 21:36:42 -0700

    Aun trabajando...

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ var HelloWorldLayer = cc.Layer.extend({
     sprConejo:null,
     sprBomba:null,
     sprZanahoria:null,
+    random: function getRandomInt(min, max) {
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	},
     ctor:function () {
         this._super();
         //Obteniendo el tamaño de la pantalla
@@ -21,16 +24,16 @@ var HelloWorldLayer = cc.Layer.extend({
         
         //posicionando las bombas
         this.sprBomba = new cc.Sprite(res.bomba_png);
-        this.sprBomba.setPosition(size.width / 2,size.height * 0.8);
-        this.addChild(this.sprBomba, 1);
-        var bomba_action= cc.MoveTo.create(2, cc.p(50, 100));
+        this.sprBomba.setPosition(size.width / 1.4,size.height / 1.1);
+        this.addChild(this.sprBomba, 2);
+        var bomba_action= cc.MoveTo.create(3, cc.p(size.width / 1.4,size.height * -1));
         this.sprBomba.runAction(bomba_action);
         
         //posicionando las zanahorias
         this.sprZanahoria = new cc.Sprite(res.zanahoria_png);
-        this.sprZanahoria.setPosition(size.width/2,size.height/2);
+        this.sprZanahoria.setPosition(size.width/2.7,size.height/1.1);
         this.addChild(this.sprZanahoria, 1);
-        var zanahoria_action= cc.MoveTo.create(2, cc.p(50, 100));
+        var zanahoria_action= cc.MoveTo.create(3, cc.p(size.width / 2.5,size.height * -1));
         this.sprZanahoria.runAction(zanahoria_action);
 
 
